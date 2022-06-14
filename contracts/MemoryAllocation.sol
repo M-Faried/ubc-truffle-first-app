0d66b21b2d88a3f5ab49c8369ecdc70e7e9fb7b8
M Faried <m.a.faried@gmail.com>
2022-06-14 14:12:03 +0200

    A code demonstrates different types of variables from the memory point of view

---
+++ contracts/MemoryAllocation.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+contract MemoryAllocation {
+
+    // The following are called state variables and are allocated in storage.
+    uint count;
+    uint[] points;
+
+
+    function localVariables() {
+        uint[] localArray; // If you don't storage, it will be created in storage by default in previous versions.
+        uint[] storage localStorageArray;
+        uint[] memory  memoryArray; // Located in the memory and not the storage.
+
+        // Creates a reference to the storage array.
+        uint[] memory pointer = points;
+    }
+}
